f810cb9cd903bac4fcb70d91a1751f91cebccf71
Str-Gen <laurens.dhooge@telenet.be>
2018-07-03 21:44:25 +0200

    With indentation working in VsCode

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,38 +1,46 @@
 #include <iostream>
 
-using std::endl;
-using std::cout;
 using std::cin;
+using std::cout;
+using std::endl;
 
-class Calculator {
-    public:
-    int Calculate(int,int,char);
+class Calculator
+{
+  public:
+    int Calculate(int, int, char);
 };
 
-int main(){
-    int x,y,result;
+int main()
+{
+    int x, y, result;
     char oper;
-    cout<<"Hello I'm a calculator"<<endl;
-    cout<<"Please enter num1 operator num2: "<<endl;
+    cout << "Hello I'm a calculator" << endl;
+    cout << "Please enter num1 operator num2: " << endl;
     cin >> x >> oper >> y;
     Calculator c;
-    result = c.Calculate(x,y,oper);
+    result = c.Calculate(x, y, oper);
     cout << "Result is: " << result << endl;
-
 
     cin.ignore();
     cin.get();
     return 0;
 }
 
-int Calculator::Calculate(int x, int y, char oper) {
-    switch (oper){
-        case '+':return x+y;
-        case '-':return x-y;
-        case '*':return x*y;
-        case '/': if(y!=0)return x/y;
-        default:
+int Calculator::Calculate(int x, int y, char oper)
+{
+    switch (oper)
+    {
+    case '+':
+        return x + y;
+    case '-':
+        return x - y;
+    case '*':
+        return x * y;
+    case '/':
+        if (y != 0)
+            return x / y;
+    default:
         cin >> x >> oper >> y;
-        Calculate(x,y,oper);
+        Calculate(x, y, oper);
     }
 }
