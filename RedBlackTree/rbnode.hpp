20cd61e8eb4ae422d2e5d2542151531df0442eda
Str-Gen <laurens.dhooge@telenet.be>
2018-08-03 17:13:49 +0200

    red black tree node

---
+++ RedBlackTree/rbnode.hpp
@@ -0,0 +1,106 @@
+#ifndef __RBNODE_HPP
+#define __RBNODE_HPP
+
+#include "rbtree.hpp";
+
+enum class Color
+{
+    RED,
+    BLACK
+};
+
+template <class Key, class Data>
+class RBnode
+{
+    friend class RBtree<Key, Data>;
+
+  public:
+    RBnode() : parent(0)
+    {
+        //cout << "RBnode()" << endl;
+    }
+    RBnode(const Key &sl, const Data &d, const Color &c) : key{sl}, data{d}, color{c}, parent{0}
+    {
+        //cout << "RBnode(const Key &, const Data &)" << endl;
+    }
+    RBnode(Key &&sl, Data &&d, Color &&c) : key{move(sl)}, data{move(d)}, color{move(c)}, parent{0}
+    {
+        //cout << "RBnode(Key &&,Data &&)" << endl;
+    }
+    virtual ~RBnode() = default;
+
+    RBnode(const RBnode<Key, Data> &other) : key{other.key}, data{other.data}, color{other.color}
+    {
+        parent = other.parent ? other.parent : nullptr;
+        if (other.left)
+        {
+            other.left->parent = this;
+        }
+        if (other.right)
+        {
+            other.right->parent = this;
+        }
+    }
+
+    RBnode<Key, Data> &operator=(const RBnode<Key, Data> &other)
+    {
+        key = other.key;
+        data = other.data;
+        color = other.color;
+        parent = other.parent ? other.parent : nullptr;
+        if (other.left)
+        {
+            other.left->parent = this;
+        }
+        if (other.right)
+        {
+            other.right->parent = this;
+        }
+
+        return *this;
+    }
+    RBnode(RBnode<Key, Data> &&other) = delete;
+    RBnode<Key, Data> &operator=(RBnode<Key, Data> &&other) = delete;
+
+    bool operator==(const RBnode<Key,Data>& other) const {
+        return ((key==other.key)&&(data==other.data)&&(color=other.color));
+    }
+
+    bool operator!=(const RBnode<Key,Data>& other) const {
+        return !(*this == other);
+    }
+
+    const Data &getData() const
+    {
+        return data;
+    }
+
+    const Key &getKey() const
+    {
+        return key;
+    }
+
+    const Color &getColor() const
+    {
+        return color;
+    }
+
+    RBtree<Key, Data> getChild(bool l)
+    {
+        return l ? left : right;
+    }
+
+    RBtree<Key,Data>* getChild(bool l)
+    {
+        return l ? &left : &right
+    }
+
+  protected:
+    Key key;
+    Data data;
+    Color color;
+    RBnode<Key, Data> *parent;
+    RBtree<Key, Data> left, right;
+};
+
+#endif
