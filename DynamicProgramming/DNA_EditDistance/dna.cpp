002b75949019681794160d7b2ddeab9e10a75c3a
Str-Gen <laurens.dhooge@telenet.be>
2018-08-12 17:48:36 +0200

    working top down dna

--- DynamicProgramming/DNA_EditDistance/dna.cpp
+++ DynamicProgramming/DNA_EditDistance/dna.cpp
@@ -1,6 +1,49 @@
 #include "dna.hpp"
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <chrono>
 
-int main(int argc, char **argv) {
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    vector<DNAsequence> chimp_dna;
+    ifstream file{"chimp.dna"};
+    DNAsequence chimp;
+    while (file >> chimp)
+    {
+        chimp_dna.push_back(chimp);
+    }
+    file.close();
+    file.clear();
+
+    file.open("human.dna");
+    DNAsequence human;
+    auto start_time = chrono::steady_clock::now();
+    int i = 0;
+    while (file >> human)
+    {
+        int min_index = 0;
+        int min_distance = numeric_limits<int>::max();
+        for (int j = 0; j < static_cast<int>(chimp_dna.size()); j++)
+        {
+            int distance = human.d_top_down(chimp_dna[j]);
+            if (distance < min_distance)
+            {
+                min_distance = distance;
+                min_index = j;
+            }
+        }
+        cout << "Human " << i << " matches chimp " << min_index << " with distance " << min_distance << endl;
+        i++;
+    }
+
+    auto finish_time = chrono::steady_clock::now();
+    chrono::duration<double> diff = finish_time -start_time;
+
+    file.close();
+    cout<<"Total time needed for DNA comparisons: "<<diff.count()<<" s"<<endl;
+
     return 0;
 }
--- DynamicProgramming/DNA_EditDistance/dna.hpp
+++ DynamicProgramming/DNA_EditDistance/dna.hpp
@@ -5,21 +5,69 @@
 #include <string>
 using std::string;
 #include <vector>
+using std::vector;
 #include <iomanip>
 #include <algorithm>
 
-class DNAsequence: public string {
-    public:
+class DNAsequence : public string
+{
+  public:
     DNAsequence() = default;
-    DNAsequence(const string& content): string{content}{};
-    DNAsequence(string && content) : string{content} {};
+    DNAsequence(const string &content) : string{content} {};
+    DNAsequence(string &&content) : string{move(content)} {};
     virtual ~DNAsequence() = default;
-    DNAsequence(const DNAsequence& other) = default;
-    DNAsequence& operator=(const DNAsequence& other) = default;
-    DNAsequence(DNAsequence&& other) = default;
-    DNAsequence& operator=(DNAsequence&& other) = default;
+    DNAsequence(const DNAsequence &other) = default;
+    DNAsequence &operator=(const DNAsequence &other) = default;
+    DNAsequence(DNAsequence &&other) = default;
+    DNAsequence &operator=(DNAsequence &&other) = default;
 
-    
+    int d_td(const string &) const;
+    int d_bu(const string &) const;
+
+    int d_top_down(const DNAsequence &) const;
+    int d_top_down_op(const DNAsequence &, vector<vector<int>> &, int, int) const;
+
+    int d_bottom_up(const DNAsequence &) const;
 };
+
+int DNAsequence::d_td(const string& other) const {    
+   return d_top_down(DNAsequence{other});    
+}
+
+int DNAsequence::d_top_down(const DNAsequence &other) const
+{
+    const size_t FROM_LENGTH = size() + 1;
+    const size_t TO_LENGTH = other.size() + 1;
+    vector<vector<int>> distances(FROM_LENGTH, vector<int>(TO_LENGTH, -1));
+    int distance = d_top_down_op(other, distances, FROM_LENGTH - 1, TO_LENGTH - 1);
+    return distance;
+}
+
+int DNAsequence::d_top_down_op(const DNAsequence &other, vector<vector<int>> &distances, int from, int to) const
+{
+    if (distances[from][to] < 0)
+    {
+        if (from == 0)
+        {
+            distances[from][to] = to;
+        }
+        else if (to == 0)
+        {
+            distances[from][to] = from;
+        }
+        else if ((*this)[from - 1] == other[to - 1])
+        { // if the letters at the locations are the same, you can move diagonally
+            distances[from][to] = d_top_down_op(other, distances, from - 1, to - 1);
+        }
+        else
+        {
+            int additions = 1 + d_top_down_op(other, distances, from, to - 1);
+            int deletions = 1 + d_top_down_op(other, distances, from - 1, to);
+            int changes = 1 + d_top_down_op(other, distances, from - 1, to - 1);
+            distances[from][to] = std::min({additions, deletions, changes});
+        }
+    }
+    return distances[from][to];
+}
 
 #endif
